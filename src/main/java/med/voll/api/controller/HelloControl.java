d12c1d74a5e6b7661b64d0a25ae6b102b86c3b43
robinbenitezmora <robinbenitez1990@gmail.com>
2023-08-28 22:14:57 -0500

    Update 'HelloControl' file

--- src/main/java/med/voll/api/controller/HelloControl.java
+++ src/main/java/med/voll/api/controller/HelloControl.java
@@ -1,10 +1,11 @@
 package med.voll.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("hello")
+@RequestMapping("/hello")
 
 public class HelloControl {
  @GetMapping
