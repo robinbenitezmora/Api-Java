218bb14ff487ad5d344ecbe5a932ab22a7f60a1e
robinbenitezmora <robinbenitez1990@gmail.com>
2023-08-29 21:43:49 -0500

    Connect '/medics' with Insomnia

---
+++ src/main/java/med/voll/api/controller/MedicController.java
@@ -0,0 +1,16 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medics")
+public class MedicController {
+ @PostMapping
+ public void registerMedic(@RequestBody String parameter) {
+  System.out.println("Medic registered!");
+  System.out.println(parameter);
+ }
+}
