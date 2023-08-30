e2faa902b20b5f1c33397e96ab712fefd0d7b62a
robinbenitezmora <robinbenitez1990@gmail.com>
2023-08-29 21:00:24 -0500

    Fix connection on port 8080 for 'HelloWorld'

--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 
-public class HelloControl {
+public class HelloController {
  @GetMapping
  public String helloWorld() {
   return "Hello World Spring!";
