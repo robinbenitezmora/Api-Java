3fd0a01de46449eadfcf3920539148289e38f542
robinbenitezmora <robinbenitez1990@gmail.com>
2023-09-04 20:33:09 -0500

    Create again the table code for medics

---
+++ src/main/resources/db.migration/V1__create-table-medics.sql
@@ -0,0 +1,14 @@
+
+create table medics
+(
+ id bigint not null primary key,
+ name varchar(255) not null,
+ email varchar(255) not null unique,
+ document varchar(255) not null unique,
+ specialicity varchar(255) not null,
+ street varchar(255),
+ distrit varchar(255) not null,
+ compliment varchar(255),
+ number varchar(255),
+ city varchar(255) not null
+);
