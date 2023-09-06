fd00f633fc021d06dc507828d263ef6318b406b3
robinbenitezmora <robinbenitez1990@gmail.com>
2023-09-05 21:54:46 -0500

    Modify 'MedicController.java' class

--- src/main/java/med/voll/api/controller/MedicController.java
+++ src/main/java/med/voll/api/controller/MedicController.java
@@ -3,13 +3,11 @@ package med.voll.api.controller;
 import med.voll.api.medic.DataRegisterMedic;
 import med.voll.api.medic.Medic;
 import med.voll.api.medic.MedicRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import jakarta.validation.Valid;
 
 @RestController
