3fc2a67e53853de7b76335d3c4a82f1ec6fea959
robinbenitezmora <robinbenitez1990@gmail.com>
2023-09-10 16:19:03 -0500

    Create 'delete' method in the CRUD

--- src/main/java/med/voll/api/controller/MedicController.java
+++ src/main/java/med/voll/api/controller/MedicController.java
@@ -12,7 +12,9 @@ import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.P
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -45,5 +47,12 @@ public class MedicController {
   Medic medic = medicRepository.getReferenceById(dataUpdateMedic.id());
   medic.updateData(dataUpdateMedic);
   medicRepository.save(medic);
+ }
+
+ @DeleteMapping("/{id}")
+ @Transactional
+ public void deleteMedic(@PathVariable Long id) {
+  Medic medic = medicRepository.getReferenceById(id);
+  medicRepository.delete(medic);
  }
 }
