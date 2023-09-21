19f4c6164f6da35275249ebcaaaae99e55717930
robinbenitezmora <robinbenitez1990@gmail.com>
2023-09-20 22:20:41 -0500

    Refactor 'MedicController.java' file

--- src/main/java/med/voll/api/domain/medic/MedicRepository.java
+++ src/main/java/med/voll/api/domain/medic/MedicRepository.java
@@ -1,11 +1,12 @@
 package med.voll.api.domain.medic;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicRepository extends JpaRepository<Medic, Long> {
 
- Page<Medic> findByActiveTrue(Pageable pagination);
+ Object findByActiveTrue = null;
+
+ Page<Medic> findByActiveTrue(org.springframework.data.domain.Pageable pagination);
 
 }
