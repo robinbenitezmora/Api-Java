af33d03365d6af13fd1eab90f7924a8079c8dc08
robinbenitezmora <robinbenitez1990@gmail.com>
2023-09-22 20:51:34 -0500

    Update the controller

---
+++ src/main/java/med/voll/api/controller/DataJWTToken.java
@@ -0,0 +1,14 @@
+package med.voll.api.controller;
+
+public class DataJWTToken {
+ private String token;
+
+ public DataJWTToken(String token) {
+  this.token = token;
+ }
+
+ public String getToken() {
+  return token;
+ }
+
+}
