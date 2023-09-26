b342722e8ef732a9363195bc64530949e6b819f1
robinbenitezmora <robinbenitez1990@gmail.com>
2023-09-25 21:14:49 -0500

    Refactor 'ErrorTreatment.java' file

--- src/main/java/med/voll/api/infra/errors/ErrorTreatment.java
+++ src/main/java/med/voll/api/infra/errors/ErrorTreatment.java
@@ -1,6 +1,9 @@
 package med.voll.api.infra.errors;
 
 import jakarta.persistence.EntityNotFoundException;
+
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,12 +14,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ErrorTreatment {
 
  @ExceptionHandler(EntityNotFoundException.class)
- public ResponseEntity handleError404() {
+ public ResponseEntity<Void> handleError404() {
   return ResponseEntity.notFound().build();
  }
 
  @ExceptionHandler(MethodArgumentNotValidException.class)
- public ResponseEntity handleError400(MethodArgumentNotValidException exception) {
+ public ResponseEntity<List<DataErrorValidation>> handleError400(MethodArgumentNotValidException exception) {
   var dataErrors = exception.getFieldErrors().stream().map(DataErrorValidation::new).toList();
   return ResponseEntity.badRequest().body(dataErrors);
  }
